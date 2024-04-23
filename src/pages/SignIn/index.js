57ae5d248b96f54d6247894c09b31644e9d4ad5b
Valderlan Silva <valderlanjosr15@gmail.com>
2024-04-23 00:15:05 -0300

    criado a pagina de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+import './signin.css';
+
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
 export default function SignIn() {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+
     return (
-        <div>
-            <h1>Pagina Login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='Logo do sistema d chamados' />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+                        <input 
+                          type='text' 
+                          placeholder='Seu email' 
+                          value={email}
+                          onChange={ (e) => setEmail(e.target.value)}
+                        />
+
+                        <input 
+                          type='password' 
+                          placeholder='*********' 
+                          value={password}
+                          onChange={ (e) => setPassword(e.target.value)}
+                        />
+
+                        <button type='submit'>Acessar</button>
+                </form>
+
+                <Link to="/register">Criar um conta</Link>
+            </div>
         </div>
     )
 }
